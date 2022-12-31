0745a3e75909c4f3640c25be235efe7b273ec2e4
Forphiper <cilin.joey@gmail.com>
2022-12-31 23:11:17 +0800

    Add files needed for the project

---
+++ hw4/cpu.f
@@ -0,0 +1,18 @@
+// testbench
+./testbench.v
+
+// Your files
+./codes/cpu.v
+./codes/instruction_memory.v
+./codes/data_memory.v
+./codes/alu.v
+./codes/alu_control.v
+./codes/control.v
+./codes/imm_gen.v
+./codes/mux.v
+./codes/register_file.v
+./codes/forwarding_unit.v
+./codes/hazard_detection_unit.v
+./codes/adder.v
+./codes/shift_left_one.v
+./codes/pc_source.v
